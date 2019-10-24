c4ff18d52388b935a14bee6bab9f4d5ff9a48029
Philip J. Ball <philipjball93@gmail.com>
2019-10-24 16:39:24 +0100

    start code

---
+++ model.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+
+class NeuralProcess():
+    def __init__(self):
+        self.encoder = self.build_encoder()
+        self.decoder = self.build_decoder()
+
+    def build_encoder(self):
+
+    def build_decoder(self):
+        
