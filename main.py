7d6d8b106b5165a12644a2fe4d1cb660f83ef14e
Philip Ball <philipjball93@gmail.com>
2019-11-06 01:05:22 +0000

    quick fixes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import numpy as np
 from sklearn import gaussian_process as gp
 import torch
 import torch.nn as nn
-from .model import *
+from .model import NeuralProcess, NeuralProcessLoss
 
 def main():
     f = gp.GaussianProcessRegressor()
     x = np.linspace(-1, 1).reshape(-1, 1)
     y = f.sample_y(x, 20)
 
-    np = NeuralProcess()
-    loss = NeuralProcessLoss(np)
+    neural_process = NeuralProcess()
+    loss = NeuralProcessLoss(neural_process)
+
