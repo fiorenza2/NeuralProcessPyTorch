8287f4b04c420de3148200d749f87d3e9fbab7ab
Philip Ball <philipjball93@gmail.com>
2019-11-06 01:02:00 +0000

    find GP, figure out sampling, start NP in main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import numpy as np
+from sklearn import gaussian_process as gp
 import torch
 import torch.nn as nn
+from .model import *
+
+def main():
+    f = gp.GaussianProcessRegressor()
+    x = np.linspace(-1, 1).reshape(-1, 1)
+    y = f.sample_y(x, 20)
+
+    np = NeuralProcess()
+    loss = NeuralProcessLoss(np)
