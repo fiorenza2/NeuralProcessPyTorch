6ca47a47383f5b7f030eb7772e67242fc497fb40
Philip Ball <philipjball93@gmail.com>
2019-11-27 11:57:20 +0000

    tidy up, create trainer class

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
-import numpy as np
-from sklearn import gaussian_process
-from sklearn.model_selection import train_test_split
-import torch
-import torch.nn as nn
 from neuralprocess import NeuralProcess, NeuralProcessLoss
+from trainer import NeuralProcessTrainer
 from utils import GPDataGenerator
-
-def train(num_epochs: int, neural_process: NeuralProcess):
-    gp_data_generator = GPDataGenerator()
-    x_context, x_target, y_context, y_target = gp_data_generator.create_training_set()
-    loss_func = NeuralProcessLoss()
-
-    optimizer = torch.optim.Adam(neural_process.parameters(), lr=3e-4)
-
-    neural_process.train()
-
-    for i in range(num_epochs):
-        epoch_loss = 0
-        epoch_mse = 0
-        for x_c, x_t, y_c, y_t in zip(x_context, x_target, y_context, y_target):
-            optimizer.zero_grad()
-            loss = loss_func(neural_process, x_c, y_c, x_t, y_t)
-            epoch_loss += loss
-            epoch_mse += loss_func.mse_loss
-            loss.backward()
-            optimizer.step()
-        print('Epoch:{} Loss: {:.4f} Acc: {:.4f}'.format(
-                i, epoch_loss, epoch_mse))
-    
-    neural_process.eval()
-
-    return neural_process, gp_data_generator
 
 def main():
     num_epochs = 100
     neural_process = NeuralProcess(1, 1, 10, 10, 10, width=200)
-    neural_process, gp_data_generator = train(num_epochs, neural_process)
+    data_generator = GPDataGenerator()
+    trainer = NeuralProcessTrainer(neural_process, data_generator, num_epochs)
+    trainer.train()
 
 if __name__ == "__main__":
     main()
